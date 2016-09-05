11c5581aed24f18df4b2d3129bead7f4b10f6ea9
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2016-09-05 11:26:43 -0300

    correção em importes

--- src/br/edu/ifpb/infinitiGraphexample/MainApp.java
+++ src/br/edu/ifpb/infinitiGraphexample/MainApp.java
@@ -4,11 +4,10 @@ import java.util.Calendar;
 
 import com.infinitegraph.ConfigurationException;
 import com.infinitegraph.StorageException;
-import infinitiGraphexample.dao.GrafoDAO;
-import infinitiGraphexample.dao.InfinitiGraphFactory;
-import infinitiGraphexample.entidades.Membro;
-import infinitiGraphexample.entidades.MensagemPrivada;
-
+import br.edu.ifpb.infinitiGraphexample.dao.GrafoDAO;
+import br.edu.ifpb.infinitiGraphexample.dao.InfinitiGraphFactory;
+import br.edu.ifpb.infinitiGraphexample.entidades.Membro;
+import br.edu.ifpb.infinitiGraphexample.entidades.MensagemPrivada;
 
 public class MainApp {
 
@@ -24,81 +23,80 @@ public class MainApp {
 		Membro me3 = new Membro("Rafael3", "Masculino", 16, 16, "ralf3");
 
 		Membro me4 = new Membro("Rafael4", "Masculino", 16, 16, "ralf4");
-		
-		Membro me5 = new Membro("Rafael5", "Masculino", 16, 16, "ralf5");
 
+		Membro me5 = new Membro("Rafael5", "Masculino", 16, 16, "ralf5");
 
 		MensagemPrivada mensagemPrivada = new MensagemPrivada("Olá Conseguiu rodar o InfiniteGraph?", "Não",
 				Calendar.getInstance());
-		
+
 		MensagemPrivada mensagemPrivada2 = new MensagemPrivada("Olá Conseguiu rodar o InfiniteGraph?", "SIM",
 				Calendar.getInstance());
-		
-		MensagemPrivada mensagemPrivad3 = new MensagemPrivada("Olá Conseguiu rodar o InfiniteGraph?", "Não, é uma merda!",
-				Calendar.getInstance());
-		
-		MensagemPrivada mensagemPrivada4 = new MensagemPrivada("Olá Conseguiu rodar o InfiniteGraph?", "Não, é uma merda!",
-				Calendar.getInstance());
 
+		MensagemPrivada mensagemPrivad3 = new MensagemPrivada("Olá Conseguiu rodar o InfiniteGraph?",
+				"Não, é uma merda!", Calendar.getInstance());
+
+		MensagemPrivada mensagemPrivada4 = new MensagemPrivada("Olá Conseguiu rodar o InfiniteGraph?",
+				"Não, é uma merda!", Calendar.getInstance());
 
 		try {
 
 			GrafoDAO dao = new GrafoDAO();
 
 			dao.iniciarTransacao();
-			
+
 			dao.adicionaPessoa(me1);
 
 			System.out.println("CRIADO PESSOA 1");
-			
+
 			dao.adicionaPessoa(me2);
-			
+
 			System.out.println("CRIADO PESSOA 2");
 
 			dao.adicionaPessoa(me3);
-			
+
 			System.out.println("CRIADO PESSOA 3");
-			
+
 			dao.adicionaPessoa(me4);
-			
+
 			System.out.println("CRIADO PESSOA 4");
-			
+
 			dao.adicionaPessoa(me5);
-			
+
 			System.out.println("CRIADO PESSOA 5");
 
-			
 			dao.adicionarConexaoMensagemUnidericional(me1, me2, mensagemPrivada);
-			
+
 			dao.adicionarConexaoMensagemUnidericional(me1, me3, mensagemPrivad3);
 
 			dao.adicionarConexaoMensagemBidericional(me1, me4, mensagemPrivada2);
 
 			dao.adicionarConexaoMensagemBidericional(me4, me5, mensagemPrivada4);
-			
-			//dao.adicionarConexaoMensagemBidericional(me3, me5, mensagemPrivada);
-			//Exclui qualque instacia da classe abaixo do gráfico visual
-//			myGraphView.excludeClass(InstantChat.class);
 
-			
+			// dao.adicionarConexaoMensagemBidericional(me3, me5,
+			// mensagemPrivada);
+			// Exclui qualque instacia da classe abaixo do gráfico visual
+			// myGraphView.excludeClass(InstantChat.class);
+
 			System.out.println("ADICIONADO CONEXAO DE PESSOA 1 com PESSOA2 E ADICIONADO UMA ARESTA");
-			
+
 			dao.colocarNoComoRaiz(me1);
-			
+
 			System.out.println("COLOCADO PESSOA 1 COMO NÓ ROOT");
-			
+
 			System.out.println(dao.buscarPorNome(me2.getNome()));
-			
+
 			System.out.println("TESTE?");
-			
-		//	VertexPredicate myVertexPred = new VertexPredicate(InfinitiGraphFactory.getGraph().getTypeId(Pessoa.class.getName()), "name=='Rafael4'");
-			
+
+			// VertexPredicate myVertexPred = new
+			// VertexPredicate(InfinitiGraphFactory.getGraph().getTypeId(Pessoa.class.getName()),
+			// "name=='Rafael4'");
+
 			dao.buscarPessoasLigadasDiretamenteJSON(me1);
-			
+
 			System.out.println(dao.buscarTodos());
-			
+
 			dao.fecharConexao();
-		
+
 		} catch (StorageException e) {
 			e.printStackTrace();
 
